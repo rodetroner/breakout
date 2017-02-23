df33d3e5413b104696b7b07461ba1b0e20b81c84
rodetroner <rodetroner@gmail.com>
2017-02-23 22:57:14 +0100

    Allow ball to move

--- breakout.py
+++ breakout.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 from pygame.locals import *
 
 FPS = 60
@@ -14,6 +15,10 @@ class Ball(pygame.Rect):
         self.centery = y
         self.width = 2 * radius
         self.height = 2 * radius
+        self.direction = math.pi / 3
+        self.speed = 3
+        self.x_offset = round(self.speed * math.cos(self.direction))
+        self.y_offset = round(self.speed * math.sin(self.direction))
 
 b = Ball(40, 40, DEFAULT_BALL_RADIUS)
 
@@ -22,7 +27,9 @@ pygame.display.init()
 while True:
     clock.tick(FPS)
 
+    pygame.display.update()
     pygame.draw.rect(screen, DEFAULT_BALL_COLOR, b);
+    b.move_ip(b.x_offset, b.y_offset)
 
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
