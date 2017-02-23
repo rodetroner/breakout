05823f7162b2bd4a373845855fbc1068845625f0
rodetroner <rodetroner@gmail.com>
2017-02-23 21:41:02 +0100

    Define Ball class

--- breakout.py
+++ breakout.py
@@ -1,9 +1,17 @@
 import pygame
 
 FPS = 60
+BALL_RADIUS = 10
 
 screen = pygame.display.set_mode((1024, 768))
 clock = pygame.time.Clock()
+
+class Ball(pygame.Rect):
+    def __init__ (self, x, y):
+        self.x = x
+        self.y = y
+        self.width = 2 * BALL_RADIUS
+        self.height = 2 * BALL_RADIUS
 
 while True:
     clock.tick(FPS)
