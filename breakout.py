efb1a51662c8cac708a434184bb2faa663e9ab58
rodetroner <rodetroner@gmail.com>
2017-02-23 21:54:53 +0100

    Add static ball

--- breakout.py
+++ breakout.py
@@ -3,22 +3,28 @@ from pygame.locals import *
 
 FPS = 60
 BALL_RADIUS = 10
+BALL_COLOR = (255, 0, 0)
 
 screen = pygame.display.set_mode((1024, 768))
 clock = pygame.time.Clock()
 
 class Ball(pygame.Rect):
     def __init__ (self, x, y, radius):
-        self.x = x
-        self.y = y
+        self.centerx = x
+        self.centery = y
         self.width = 2 * radius
         self.height = 2 * radius
 
+b = Ball(40, 40, 15)
+
 while True:
     clock.tick(FPS)
-    pygame.display.flip()
+
+    pygame.draw.rect(screen, BALL_COLOR, b);
 
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == K_ESCAPE:
                 exit(0)
+
+    pygame.display.flip()
