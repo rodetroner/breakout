75335a88715cde4e69b9bf651c004da3aaab4b1b
rodetroner <rodetroner@gmail.com>
2017-02-23 21:44:57 +0100

    Fix keyboard control

--- breakout.py
+++ breakout.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.locals import *
 
 FPS = 60
 BALL_RADIUS = 10
@@ -7,11 +8,11 @@ screen = pygame.display.set_mode((1024, 768))
 clock = pygame.time.Clock()
 
 class Ball(pygame.Rect):
-    def __init__ (self, x, y):
+    def __init__ (self, x, y, radius):
         self.x = x
         self.y = y
-        self.width = 2 * BALL_RADIUS
-        self.height = 2 * BALL_RADIUS
+        self.width = 2 * radius
+        self.height = 2 * radius
 
 while True:
     clock.tick(FPS)
