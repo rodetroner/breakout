e9ba2b650447a42300332df5abe79d9ae8a982fb
rodetroner <rodetroner@gmail.com>
2017-02-26 14:58:40 +0100

    Make control of paddle smoother

--- breakout.py
+++ breakout.py
@@ -15,8 +15,7 @@ clock = pygame.time.Clock()
 class Ball(pygame.Rect):
 
     direction = math.pi / 3
-    speed = 7
-
+    speed = 7 
     def __init__(self, x, y, radius):
         self.centerx = x
         self.centery = y
@@ -56,17 +55,11 @@ x_paddle_offset = 0
 pygame.display.init()
 
 while True:
+
     screen.fill((0,0,0))
 
     pygame.draw.rect(screen, DEFAULT_BALL_COLOR, b)
     pygame.draw.rect(screen, DEFAULT_PADDLE_COLOR, p)
-    pygame.display.update(b)
-    pygame.display.update(p)
-
-    x_ball_offset = round(b.speed * math.cos(b.direction))
-    y_ball_offset = round(b.speed * math.sin(b.direction))
-    b.move_ip(x_ball_offset, y_ball_offset)
-    p.move_ip(x_paddle_offset, 0)
 
     if (b.top <= screen_rect.top or b.bottom >= screen_rect.bottom):
         b.direction = -b.direction
@@ -82,18 +75,26 @@ while True:
     if p.right >= screen_rect.right:
         p.setX(screen_rect.right - p.width/2)
 
+    keys = pygame.key.get_pressed()
+
+    if keys[K_LEFT]:
+        x_paddle_offset = -p.speed
+    elif keys[K_RIGHT]:
+        x_paddle_offset = p.speed
+    else:
+        x_paddle_offset = 0
+
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
-            if event.key == K_LEFT:
-                x_paddle_offset = -p.speed
-            if event.key == K_RIGHT:
-                x_paddle_offset = p.speed
             if event.key == K_ESCAPE:
                 pygame.display.quit()
                 exit(0)
-        elif event.type == pygame.KEYUP:
-            if event.key == K_LEFT or event.key == K_RIGHT:
-                x_paddle_offset = 0
+
+    x_ball_offset = round(b.speed * math.cos(b.direction))
+    y_ball_offset = round(b.speed * math.sin(b.direction))
+    b.move_ip(x_ball_offset, y_ball_offset)
+    p.move_ip(x_paddle_offset, 0)
+
 
     pygame.display.flip()
 
